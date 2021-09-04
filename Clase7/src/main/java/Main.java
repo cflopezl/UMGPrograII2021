3fbb4c06f13bcdc0f6f3d539f4c50aaa3a10ac44
Christian López <cflopezl@gmail.com>
2021-09-04 07:58:35 -0600

    Clase 8 version 1

--- Clase7/src/main/java/Main.java
+++ Clase7/src/main/java/Main.java
@@ -1,10 +1,17 @@
 import utils.Bitacora;
+import utils.Correo;
 
 public class Main {
     public static void main(String[] args) {
 
-        try{
+        /*try{
             int i = 5/0;
+            int arreglo[] = new int[5];
+            arreglo[6]=20;
+        }catch (ArithmeticException ex){
+            Bitacora.getInstance().error("Ocurrio un error en la clase Main, metodo main:"+ex.getMessage());
+        }catch (IndexOutOfBoundsException ex){
+            Bitacora.getInstance().error("Ocurrio un error en la clase Main, metodo main:"+ex.getMessage());
         }catch (Exception ex){
             Bitacora.getInstance().error("Ocurrio un error en la clase Main, metodo main:"+ex.getMessage());
         }
@@ -17,7 +24,9 @@ public class Main {
         Bitacora.getInstance().info("Mostrando valores");
         System.out.println("atleta1 = " + atleta1);
         System.out.println("atleta2 = " + atleta2);
-        System.out.println("atleta3 = " + atleta3);
+        System.out.println("atleta3 = " + atleta3);*/
+
+        Correo.sendFromGMail("Saludo", "Prueba de envío de correo electrónico");
 
 
     }
---
+++ Clase7/src/main/java/utils/VariablesGlobales.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class VariablesGlobales {
+    public static final String USER_NAME = "cursocflopezl@gmail.com";  // GMail user name (just the part before "@gmail.com")
+    public static final String PASSWORD = "Curs02@21"; // GMail password
+    public static final String RECIPIENT[] = {"clopezl@miumg.edu.gt","cmoralesm24@miumg.edu.gt","kgaliciac@miumg.edu.gt","ngomezh2@miumg.edu.gt","jjolone1@miumg.edu.gt"};
+    public static final int INFO = 7;
+    public static final int MEDIUM = 5;
+    public static final int DANGER = 3;
+}
